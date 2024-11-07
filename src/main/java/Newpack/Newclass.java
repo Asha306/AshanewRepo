8cea5cab3f75ac98e9a1d9aa9fb23059871dd705
USER <USER@DESKTOP-7SM6G84>
2024-11-07 06:44:26 +0530

    new

---
+++ src/main/java/Newpack/Newclass.java
@@ -0,0 +1,10 @@
+package Newpack;
+
+public class Newclass {
+public static void main(String[] args) {
+	
+	System.out.println("hiii");
+	System.out.println("hello");
+	System.out.println("good");
+}
+}
